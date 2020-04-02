544af8c241b91f112c23696435c0112ffbd6d371
trentdouglas99 <trentdouglas@mymail.mines.edu>
2020-04-02 16:33:35 -0600

    added computer player target tests (failing)

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -3,6 +3,23 @@
  */
 package clueGame;
 
+import java.util.Set;
+
 public class ComputerPlayer extends Player {
+	
+	private BoardCell visited;
+	
+	public BoardCell getVisited() {
+		return visited;
+	}
+
+	public void setVisited(BoardCell visited) {
+		this.visited = visited;
+	}
+
+	public BoardCell pickLocation( Set<BoardCell> targets) {
+		
+		return null;
+	}
 	
 }
